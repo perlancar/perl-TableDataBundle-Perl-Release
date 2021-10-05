71f9cf683343646b685d8d25dbc34dfeab55530b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-10-05 18:25:04 +0700

    Initial commit

---
+++ lib/TableDataBundle/Perl/Release.pm
@@ -0,0 +1,28 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package TableDataBundle::Perl::Release;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of TableData:: modules that contain Perl releases data
+
+=head1 DESCRIPTION
+
+B<NOT YET IMPLEMENTED. PLACEHOLDER ONLY.>
+
+This distribution contains the following L<TableData>:: modules:
+
+# INSERT_MODULES_LIST /^TableData::/
+
+
+=head1 SEE ALSO
+
+L<TableData>
+
+L<TableDataBundle::Perl::CPAN::Release>
+
+=cut
